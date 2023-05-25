66c6ff34e4097b39874e96ac004201112a27c7c8
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-25 19:37:31 +0530

    Developed the basic UI components of and form submit function

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import PostCreate from "./PostCreate";
+
+export default () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
